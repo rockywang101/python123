b796a5f9b87836ca2133e5b528e3e902d0f3729e
rocky.wang <rockywang101@gmail.com>
2018-03-21 18:46:30 +0800

    1

--- first.py
+++ first.py
@@ -7,3 +7,17 @@ import os
 def hello(token, ar1):
     pass
 
+
+
+with open("c:\\users\\eit\\downloads\\3.20-21.txt") as f1:
+    lines = f1.read().splitlines()
+    
+print(len(lines))
+
+msg = ""
+for line in lines:
+    msg += line + ", "
+
+print(msg)
+    
+        
--- googlesheet/googlesheetDemo02_not_decide_yet.py
+++ googlesheet/googlesheetDemo02_not_decide_yet.py
@@ -1,26 +1,25 @@
 '''
-create Google Sheet new range
+get Google Sheet all ranges data
  
 Created on 2018年3月16日
 @author: rocky.wang
 '''
+from __future__ import print_function
+
+import json
 from googlesheet.googlesheetUtils import buildService
+
 
 def main():
     service = buildService()
-    spreadsheetId = "1CxCM_fOzFAySeg7pDs5g0SPCHOkxJFGQaisru3iC6Lw"
-    rangeName = "TEST"
+    spreadsheetId = "1bydUUYVTxyyz2jxm3JRzIQkdElNthLHuQpzW6-DLVUk"
     
-    result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=rangeName).execute()
-    values = result.get('values', [])
-
-    for row in values:
-        print(row)
+    ranges = [] # empty list means get all ranges data
+    request = service.spreadsheets().get(spreadsheetId=spreadsheetId, ranges=ranges, includeGridData=False)
+    response = request.execute()
+    print(response)
+    print("%s" %(json.dumps(response, indent=4)))
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
