1ed90721bd4eed67e34146697f0c44369d94884e
rocky.wang <rockywang101@gmail.com>
2018-03-29 18:44:47 +0800

    1

---
+++ date00/dateDemo02.py
@@ -0,0 +1,20 @@
+'''
+Created on 2018年3月29日
+@author: rocky.wang
+'''
+from datetime import datetime
+
+now = datetime.now()
+
+print(now)
+
+print(now.year)
+print(now.month)
+print(now.day)
+
+print(now.hour)
+print(now.minute)
+print(now.second)
+print(now.microsecond)
+
+
