fe8c2a7c49b65c8cfe283c690633fb842d5b7919
rocky.wang <rockywang101@gmail.com>
2018-04-23 19:09:02 +0800

    1

--- selenium00/seleniumDemo04.py
+++ selenium00/seleniumDemo04.py
@@ -7,16 +7,35 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 import datetime
+from selenium.webdriver.common.keys import Keys
+import os
 
 driver = webdriver.Firefox()
 
-driver.get("https://www.facebook.com/lifeinvestment168/")
+driver.get("https://www.facebook.com/")
 
-while True:
-    
-    print(driver.page_source)
-     
-    soup = BeautifulSoup(driver.page_source, "html.parser")
+elem = driver.find_element_by_id("email")
+elem.clear()
+elem.send_keys("rockywang101@gmail.com")
 
-    print()    
-    time.sleep(30)
+elem = driver.find_element_by_id("pass")
+elem.clear()
+elem.send_keys(os.environ["MY_PASSWORD"])
+
+elem.send_keys(Keys.RETURN)
+
+time.sleep(5)
+
+driver.get("https://www.facebook.com/greenbookstore/")
+
+
+# while True:
+#     
+# #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#     
+#     print(driver.page_source)
+#      
+#     soup = BeautifulSoup(driver.page_source, "html.parser")
+# 
+#     print()    
+#     time.sleep(60)
