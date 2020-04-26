f4f045abb890429725e2924a504ca23716a4cec6
rocky.wang <rocky.wang@ehsn.com.tw>
2020-04-26 12:44:03 +0800

    add comment

--- selenium00/seleniumDemo01.py
+++ selenium00/seleniumDemo01.py
@@ -1,12 +1,26 @@
 '''
+
+1. download geckodriver.exe from https://github.com/mozilla/geckodriver/releases
+2. put it in a folder
+3. add this folder into your PATH  
+4. remember to restart eclipse
+5. run ! 
+
+20200426 try firefox
+Unable to find a matching set of capabilities
+
+so i change to using Chrome
+
 Created on 2018年4月20日
 @author: rocky.wang
 '''
 
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
+# driver = webdriver.Firefox()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
@@ -15,3 +29,4 @@ elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
 driver.close()
+driver.quit()
