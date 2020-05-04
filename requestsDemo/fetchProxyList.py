0aaeea2ff0753eca7b12fc3f43459cd7bf17d80c
rocky.wang <rocky.wang@ehsn.com.tw>
2020-05-04 15:26:53 +0800

    proxy list modify

--- requestsDemo/fetchProxyList.py
+++ requestsDemo/fetchProxyList.py
@@ -1,6 +1,8 @@
 '''
 crawl Proxy List from https://www.us-proxy.org/
 
+tip: 網站上 proxy list 的資料會一直更新，取最新的應該是最有效的
+
 Created on 2020年5月4日
 @author: rocky
 '''
@@ -10,11 +12,14 @@ import csv
 
 def fetch():
 
-#     r = requests.get("https://www.us-proxy.org/")
+    r = requests.get("https://www.us-proxy.org/")
+    soup = BeautifulSoup(r.text, "html.parser")
+    
+    # when debug, don't call everytime
 #     with open("proxyList.html", "w", encoding="utf-8") as f:
 #         f.write(r.text)
-    with open("proxyList.html", encoding="utf-8") as f:
-        soup = BeautifulSoup(f.read(), "html.parser")
+#     with open("proxyList.html", encoding="utf-8") as f:
+#         soup = BeautifulSoup(f.read(), "html.parser")
 
     rowList = []
     rowList.append(["IP Address", "Port", "Code", "Country", "Anonymity", "Google", "Https", "Last Checked"])
@@ -24,9 +29,14 @@ def fetch():
             row.append(td.text)
         rowList.append(row)
     
-    with open("proxyList.csv", "w", encoding="utf-8") as f:
-        csv.writer(f).writerows(rowList)
+    with open("proxyList.csv", "w", newline="", encoding="utf-8") as f:
+        wr = csv.writer(f)
+        wr.writerows(rowList)
 
+    print(rowList[0])
+    print(rowList[1])
+    print(rowList[-1])
+    print(f"proxy list: {len(rowList)}")
     print("completed")
     
 if __name__ == "__main__":
