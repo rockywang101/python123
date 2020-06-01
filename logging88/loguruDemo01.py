8a1898473ee75f04a36237c8490000862c6fdca1
rocky.wang <rocky.wang@ehsn.com.tw>
2020-06-01 15:10:41 +0800

    add loguru demo

---
+++ logging88/loguruDemo01.py
@@ -0,0 +1,13 @@
+'''
+Created on 2018年10月9日
+@author: rocky.wang
+'''
+from loguru import logger
+
+logger.trace('I am TRACE Level')
+logger.debug('I am DEBUG Level')
+logger.info('I am INFO Level')
+logger.success('I am SUCCESS Level')
+logger.warning('I am WARNING Level')
+logger.error('I am ERROR Level')
+logger.critical('I am CRITICAL Level')
---
+++ logging88/loguruDemo02.py
@@ -0,0 +1,35 @@
+'''
+Created on 2018年10月9日
+@author: rocky.wang
+'''
+
+import datetime
+
+from loguru import logger
+import sys
+
+logger.remove()
+logger.add(
+    sys.stdout, 
+    colorize=True, 
+    level='TRACE', 
+    format='<level>{time:YYYY-MM-DD HH:mm:ss} {level}\t{message}</level>'
+)
+
+logger.add(
+    f'{datetime.date.today():%Y%m%d}.log',
+    rotation='1 day',
+    retention='7 days',
+    level='TRACE',
+    format='<level>{time:YYYY-MM-DD HH:mm:ss} {level}\t{message}</level>'
+)
+
+
+logger.trace('I am TRACE Level')
+logger.debug('I am DEBUG Level')
+logger.info('I am INFO Level')
+logger.success('I am SUCCESS Level')
+logger.warning('I am WARNING Level')
+logger.error('I am ERROR Level')
+logger.critical('I am CRITICAL Level')
+
