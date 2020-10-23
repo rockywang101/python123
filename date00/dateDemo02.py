1f6e48b107fc882493e8463e21360ceffc1f9349
rocky.wang <rockywang101@gmail.com>
2020-10-23 18:01:21 +0800

    fx

--- date00/dateDemo02.py
+++ date00/dateDemo02.py
@@ -24,4 +24,25 @@ now = time.time()
 print(now)
 
 
+def add():
+    for i in range(5000000):
+        i = i + 1
+
+a = time.time()
+begin = time.perf_counter() # 已是秒數表示，不用看 timestamp 看不是很懂過了幾秒
+print(a)
+print(begin) 
+print('--------------------')
+
+add()
+b = time.time()
+middle = time.perf_counter()
+print(b-a)
+print(middle-begin)
+
+add()
+c = time.time()
+end = time.perf_counter()    
+print(f'{c-b:0.4f}')
+print(f'{end-middle:0.4f}')
 
